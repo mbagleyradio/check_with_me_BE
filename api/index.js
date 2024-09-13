fbf42278f48fcc23c9c75f6d249735f7892a357c
mbagleyradio <mbagleyradio@gmail.com>
2024-09-13 11:33:44 -0700

    dotenv

--- api/index.js
+++ api/index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+let key = process.env.API_KEY;
+let port = Number(process.env.VERCEL_PORT);
 
 // this is the part you will see
 app.get("/", (req, res) => {
-    res.send("This is the root route for GET requests on this server") });
+    
+    /*
+    const url = "https://www.searchapi.io/api/v1/search";
+    const params = new URLSearchParams({
+        "engine": "google",
+        "q": `${req.query.fName} ${req.query.lName} injury status`,
+        "api_key": `${process.env.API_KEY}`
+    });
 
-app.listen(5000, () => console.log("Your express server deployed on vercel is listening on port 5000"));
+    fetch(`${url}?${params}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    */
+   
+    res.send(`key: ${key}`); 
+});
+
+app.listen(port, () => console.log(`Your express server deployed on vercel is listening on port ${port}`));
 
 module.exports = app;
