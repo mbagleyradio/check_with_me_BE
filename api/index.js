04eb27dd5b0d9d00151f82ffd66de26172955202
mbagleyradio <mbagleyradio@gmail.com>
2024-09-12 20:39:45 -0700

    updating repo

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// this is the part you will see
+app.get("/", (req, res) => {
+    res.send("This is the root route for GET requests on this server") });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(5000, () => console.log("Your express server deployed on vercel is listening on port 5000"));
 
 module.exports = app;
