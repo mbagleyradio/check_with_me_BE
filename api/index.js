e1399daefbb7b66e04b5de680845af78acaece82
mbagleyradio <mbagleyradio@gmail.com>
2024-09-13 13:19:55 -0700

    updated vercel json for cors

--- api/index.js
+++ api/index.js
@@ -7,25 +7,21 @@ let port = Number(process.env.VERCEL_PORT);
 // this is the part you will see
 app.get("/", (req, res) => {
     
-    /*
     const url = "https://www.searchapi.io/api/v1/search";
     const params = new URLSearchParams({
         "engine": "google",
-        "q": `${req.query.fName} ${req.query.lName} injury status`,
+        "q": `${req.query.first} ${req.query.last} injury status`,
         "api_key": `${process.env.API_KEY}`
     });
 
     fetch(`${url}?${params}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        res.send(data);
     })
     .catch(error => {
         console.error('Error:', error);
     });
-    */
-   
-    res.send(`key: ${key}`); 
 });
 
 app.listen(port, () => console.log(`Your express server deployed on vercel is listening on port ${port}`));
